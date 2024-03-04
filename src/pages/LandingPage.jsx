30ea49be85c6b71a2d4f173fa229628a34521852
Rahul <rajamohanty12345@gmail.com>
2024-03-05 04:51:16 +0530

    tokenomics section added

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import Header from "../layouts/Header";
 import Team from "../components/Team";
 import Container from "../layouts/Container";
+import Tokenomics from "../components/Tokenomics";
 const LandingPage = () => {
   return (
     <>
       <Header />
       <Container>
-      <Team />
+        <Team />
+        <Tokenomics />
       </Container>
     </>
   );
