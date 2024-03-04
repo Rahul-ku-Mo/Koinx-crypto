d97222f5e146494037d8433626fc67067ecb4cc1
Rahul <rajamohanty12345@gmail.com>
2024-03-05 04:17:05 +0530

    Team section added-simple

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,15 @@
-import Header from "../layouts/Header"
-
+import Header from "../layouts/Header";
+import Team from "../components/Team";
+import Container from "../layouts/Container";
 const LandingPage = () => {
   return (
     <>
-    <Header/>
-    
+      <Header />
+      <Container>
+      <Team />
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
